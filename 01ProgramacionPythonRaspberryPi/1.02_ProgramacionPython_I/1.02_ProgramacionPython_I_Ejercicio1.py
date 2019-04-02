c4b59dc4ab79df445436231869ae9ff6a1f447ac
Alx Garcia <xelagg@gmail.com>
2019-04-02 23:00:30 +0200

    Create 02ProgramacionPythonRaspberryPi
    Create 02ProgramacionPythonRaspberryPi

---
+++ 01ProgramacionPythonRaspberryPi/1.02_ProgramacionPython_I/1.02_ProgramacionPython_I_Ejercicio1.py
@@ -0,0 +1,13 @@
+# Ejercicio 1. Variables
+# Definicion y asignacion de valores a las variables
+var_Numero_Entero=432
+var_Numero_Decimal=56.12
+var_Texto_Comillas_Dobles="Bienvenido"
+var_Texto_Comillas_Simples='Hola'
+var_Logica=True
+# Salida por pantalla de las variables
+print(var_Numero_Entero)
+print(var_Numero_Decimal)
+print(var_Texto_Comillas_Dobles)
+print(var_Texto_Comillas_Simples)
+print(var_Logica)
---
+++ 01ProgramacionPythonRaspberryPi/1.02_ProgramacionPython_I/1.02_ProgramacionPython_I_Ejercicio2.py
@@ -0,0 +1,4 @@
+# Este es un comentario al inicio de linea
+var_Saludo="Hola" # Este es un segundo comentario después de una línea de código
+                                  # Este es un tercer comentario sangrado
+var_Texto="# Esto no es un comentario, es un texto entre comillas que se asigna a una variable"
