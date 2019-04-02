34a243eb78c668b0af6d15209d9daf6e77ad5ff2
Alx Garcia <xelagg@gmail.com>
2019-04-02 23:07:12 +0200

    Create 1.03_ProgramacionPython
    Create 1.03_ProgramacionPython

---
+++ 01ProgramacionPythonRaspberryPi/1.03_ProgramacionPython_II/1.03_ProgramacionPython_II.py
@@ -0,0 +1,9 @@
+# Declaramos una variable donde se asigna el valor que introducirá el usuario (nombre)
+var_Nombre=input("Introduzca su nombre: ")
+# Declaramos una variable donde se asigna el valor que introducirá el usuario (apellido 1)
+var_Apellido1=input("Introduzca su primer apellido: ")
+# Declaramos una variable donde se asigna el valor que introducirá el usuario (apellido 2)
+var_Apellido2=input("Introduzca su segundo apellido: ")
+# Ahora mostraremos por pantalla un mensaje con el nombre completo
+var_NombreCompleto=var_Nombre+" "+var_Apellido1+" "+var_Apellido2
+print(var_NombreCompleto)
